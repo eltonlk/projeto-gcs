9297848b0261f13d2438ecac7086d6b8c0e6cc44
Elton Luis Kolling <eltonlk@gmail.com>
2021-06-18 08:23:08 -0300

    Adicionado o bootstrap e feito o menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :tags
   devise_for :users
 
   resources :bank_accounts, except: [ :show ]
+
+  resources :tags, except: [ :show ]
 
   root to: 'dashboard#index'
 end
