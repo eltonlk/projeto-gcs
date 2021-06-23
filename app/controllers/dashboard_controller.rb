0e6c3ad5ace4930e0968e0dce1590c85d856f43f
Elton Luis Kolling <eltonlk@gmail.com>
2021-06-23 16:43:01 -0300

    Adicionado o saldo por conta corrente no resumo.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < TenantController
 
   def index
+    @bank_accounts = BankAccount.all
+    @balance_by_bank_accounts = Operation.group(:bank_account_id).sum(:amount)
+
     respond_with :dashboard
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :tags, except: [ :show ]
 
-  root to: 'operations#index'
+  root to: 'dashboard#index'
 end
